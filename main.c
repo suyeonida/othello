8f7af6d9005a60e9ba930ab9547b090395f46154
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-11-22 23:16:01 +0900

    boardfunction

--- main.c
+++ main.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 6
 #include<Windows.h> 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int gameboard[N][N]={
+
+int gameboard[6][6]={
 	{0,0,0,0,0,0}
 	{0,0,0,0,0,0}
 	{0,0,1,2,0,0}
 	{0,0,2,1,0,0}
 	{0,0,0,0,0,0}
 	{0,0,0,0,0,0}
-	}
+	};
 	
-
-
-int stone(int arr[6][6]){
+int main(int argc, char *argv[]) {
+int board_function(int gameboard[6][6]){  //게임판 나오고 초기 4개 돌 배치 
 	int i,j;
-	for(i=0;i<8;i++){
-		gotoxy(2,2+2*i)  //이해 안됌.꼭 필요한가? 
+	for(i=0;i<6;i++){
 		
-		for(j=0;j<8;j++){
-			if(arr[i][j]=0)
+		for(j=0;j<6;j++){
+			if(arr[i][j]==0)
 				printf(" ");
-			else if (arr[i][j]=1)
+			else if (arr[i][j]==1)
 				printf("o");
-			else if(arr[i][j]=2)
-				printf("x")
-			
+			else if(arr[i][j]==2)
+				printf("x");
 		}
 	
-	printf("\n\n");
-	gotoxy(35,9);
-	printf("일시정지 [K]");
-	}
-	return arr[6][6];	
+	printf("\n"); }
+	return gameboard[6][6];	
 }
 
 
 int main(int argc, char *argv[]) {
+	int board_function(int gameboard[6][6]); 
 	return 0;
 }
