c3da9d3f097751e78717cfe30fb624ebc313a024
suyeonida <70572398+suyeonida@users.noreply.github.com>
2020-11-24 20:53:22 +0900

    11.24 8:53pm

---
+++ board_function_final.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+	
+//보드판 초기화 상태  
+
+int main(int argc, char *argv[]) {
+	int gameboard[6][6]={0};
+	
+	board();
+	
+	return 0;
+}
+
+int board(int gameboard[6][6]) {
+	int i,j;
+	printf("  0 1 2 3 4 5\n--------------\n");
+	for(i=0;i<6;i++){
+			printf("%d|",i);
+		for(j=0;j<6;j++){
+			printf("%d|",gameboard);  
+		}
+	printf("\n--------------\n");
+	}
+	return gameboard[6][6];
+}
+
